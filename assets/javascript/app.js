0338b2b35a7278a945a2e933b3a7fa191b0a01bc
Amanda <tranamanda14@gmail.com>
2018-07-07 06:32:08 -0500

    trivia

---
+++ assets/javascript/app.js
@@ -0,0 +1,53 @@
+//TIMER 
+
+var number = 60;
+var intervalId;
+var startbutton = document.getElementById("start");
+var submitbutton = document.getElementById("submit");
+var correctanswers = ["Teal"]
+
+
+$("document").ready(function () {
+    $("#questions").hide();
+    $("#results").hide();
+
+
+    startbutton.onclick = function() {
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+        $("#questions").show();
+    }
+
+
+    function decrement() {
+        number--;
+
+        $("#timer").html("<h2>" + number + "</h2>");
+
+        if (number === 0) {
+            stop();
+
+        }
+    }
+
+    function stop() {
+        clearInterval(intervalId);
+        $("#results").show();
+        checkanswers();
+
+    }
+    startbutton.onclick = function() {
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+        $("#questions").show();
+    }
+    $("#questions").submit(function (event) {
+        stop();
+        console.log(event)
+
+    })
+
+    function checkanswers() {
+
+    }
+})
